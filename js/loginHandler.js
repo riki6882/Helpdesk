79b09659f61f8222fe391bac83db65c8fe8f3c14
Richard Kriak <richard.kriak@gmail.com>
2026-06-25 16:16:27 +0200

    loginHandler js file, sending data to php

---
+++ js/loginHandler.js
@@ -0,0 +1,15 @@
+function getRolePassword() {
+    let role = document.getElementById('role').value;
+    let password = document.getElementById('password').value;
+}
+
+async function verifyData(role, password) {
+    if (role != '' && password != '') {
+        try {
+            const response = await fetch()
+
+        } catch (error) {
+
+        }
+    }
+}
